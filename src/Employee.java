892c37d5ab14dd53c20e25c8baefab53a8ec4eb7
aganchev <alexander.m.ganchev@gmail.com>
2023-12-04 00:49:05 +0200

    fix validate in daily report

--- src/Employee.java
+++ src/Employee.java
@@ -17,44 +17,43 @@ public class Employee extends User {
         Client.printAllClients();
         Client selectedClient = Client.returnSelectedClient(scanner);
         scanner.nextLine();
-        while (true) {
-            if (date == null) {
-                while (true) {
-                    System.out.print("Въведете дата : ");
-                    date = scanner.nextLine();
-                    try {
-                        Date validateDate = DailyReport.dateFormat.parse(date);
-                        if (date.equals(DailyReport.dateFormat.format(validateDate))) {
-                            break;
-                        } else {
-                            throw new Exception();
-                        }
-                    } catch (Exception e) {
-                        System.out.println("Въведете дата във формат дд/мм/гггг");
-                    }
-                }
-            }
+        if (date == null) {
             while (true) {
-                time = 0;
-                System.out.print("Въведете часове : ");
+                System.out.print("Въведете дата : ");
+                date = scanner.nextLine();
                 try {
-                    String input = scanner.next();
-                    time = Double.parseDouble(input);
+                    Date validateDate = DailyReport.dateFormat.parse(date);
+                    if (date.equals(DailyReport.dateFormat.format(validateDate))) {
+                        break;
+                    } else {
+                        throw new Exception();
+                    }
                 } catch (Exception e) {
-                    System.out.println("Въведен е невалиден формат за час!");
-                }
-                if (time > 8) {
-                    System.out.println("Въведени са повече от 8 часа!");
-                } else if (time < 0) {
-                    System.out.println("Въведени са отрицателни часове!");
-                } else if (time > 0) {
-                    break;
+                    System.out.println("Въведете дата във формат дд/мм/гггг");
                 }
             }
-            DailyReport dailyReport = new DailyReport(date, selectedClient.getName(), selectedClient.getProject(), activeUser.getName(), time);
-            FileHandler.writeReport(dailyReport);
-            Menu.employeeMenu(scanner);
         }
+        while (true) {
+            time = 0;
+            System.out.print("Въведете часове : ");
+            try {
+                String input = scanner.next();
+                time = Double.parseDouble(input);
+            } catch (Exception e) {
+                System.out.println("Въведен е невалиден формат за час!");
+            }
+            if (time > 8) {
+                System.out.println("Въведени са повече от 8 часа!");
+            } else if (time < 0) {
+                System.out.println("Въведени са отрицателни часове!");
+            } else if (time > 0) {
+                break;
+            }
+        }
+        DailyReport dailyReport = new DailyReport(date, selectedClient.getName(), selectedClient.getProject(), activeUser.getName(), time);
+        FileHandler.writeReport(dailyReport);
+        Menu.employeeMenu(scanner);
     }
 }
+
 
