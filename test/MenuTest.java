2c4ca54b94aab033e7dc9ffd82826b2ec6420354
aganchev <alexander.m.ganchev@gmail.com>
2023-12-13 23:46:21 +0200

    added some unit tests for Menu Class

--- test/MenuTest.java
+++ test/MenuTest.java
@@ -6,10 +6,24 @@ import java.util.Scanner;
 
 
 public class MenuTest {
-    @Test
-    void testLoginMenuReturnCorrectOutputWithIncorrectUsernameAndPassword() {
+     @Test
+    void testEmployeeMenu() {
         //GIVEN
-        ByteArrayInputStream testIn = new ByteArrayInputStream("user\n1234\n".getBytes());
+        ByteArrayInputStream testIn = new ByteArrayInputStream("user\n1234\n1\n0\n2\n".getBytes());
+        System.setIn(testIn);
+        //WHEN
+        try {
+            Menu.loginMenu(new Scanner(System.in));
+        } catch (Exception e) {
+            System.out.println();
+        }
+        //THEN
+        Assertions.assertNotNull(User.activeUser);
+    }
+    @Test
+    void testAdminMenu() {
+        //GIVEN
+        ByteArrayInputStream testIn = new ByteArrayInputStream("admin\n1\n1\n2\n0\n2\n3\n0\n3\n1\nиван иванов\n2\n49\n0\n".getBytes());
         System.setIn(testIn);
         //WHEN
         try {
