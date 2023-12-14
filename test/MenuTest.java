47a7b4f5af12ab9b5002742d7d21c5b328191f0b
aganchev <alexander.m.ganchev@gmail.com>
2023-12-14 16:47:28 +0200

    some final touches

--- test/MenuTest.java
+++ test/MenuTest.java
@@ -6,33 +6,33 @@ import java.util.Scanner;
 
 
 public class MenuTest {
-//     @Test
-//    void testEmployeeMenu() {
-//        //GIVEN
-//        ByteArrayInputStream testIn = new ByteArrayInputStream("user\n1234\n1\n0\n2\n".getBytes());
-//        System.setIn(testIn);
-//        //WHEN
-//        try {
-//            Menu.loginMenu(new Scanner(System.in));
-//        } catch (Exception e) {
-//            System.out.println();
-//        }
-//        //THEN
-//        Assertions.assertNotNull(User.activeUser);
-//    }
-//    @Test
-//    void testAdminMenu() {
-//        //GIVEN
-//        ByteArrayInputStream testIn = new ByteArrayInputStream("admin\n1\n1\n2\n0\n2\n3\n0\n3\n1\nиван иванов\n2\n49\n0\n".getBytes());
-//        System.setIn(testIn);
-//        //WHEN
-//        try {
-//            Menu.loginMenu(new Scanner(System.in));
-//        } catch (Exception e) {
-//            System.out.println();
-//        }
-//        //THEN
-//        Assertions.assertNotNull(User.activeUser);
-//    }
+     @Test
+    void testEmployeeMenu() {
+        //GIVEN
+        ByteArrayInputStream testIn = new ByteArrayInputStream("iivanov\n1234\n1\n0\n2\n".getBytes());
+        System.setIn(testIn);
+        //WHEN
+        try {
+            Menu.loginMenu(new Scanner(System.in));
+        } catch (Exception e) {
+            System.out.println();
+        }
+        //THEN
+        Assertions.assertNotNull(User.activeUser);
+    }
+    @Test
+    void testAdminMenu() {
+        //GIVEN
+        ByteArrayInputStream testIn = new ByteArrayInputStream("aganchev\n1234\n1\n2\n0\n2\n3\n0\n3\n1\nиван иванов\n2\n49\n0\n".getBytes());
+        System.setIn(testIn);
+        //WHEN
+        try {
+            Menu.loginMenu(new Scanner(System.in));
+        } catch (Exception e) {
+            System.out.println();
+        }
+        //THEN
+        Assertions.assertNotNull(User.activeUser);
+    }
 
 }
